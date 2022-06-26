0e417a0ac080942e92882cff37ed7805400cffe5
takashima-takuto <aku424tt@gmail.com>
2022-06-26 17:44:32 +0900

    pc works

--- dev/js/script.js
+++ dev/js/script.js
@@ -7,4 +7,22 @@ $(function(){
     },
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
+
+
+  const swiper = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    loop: true,
+    speed: 500,
+    slidesPerView:2,
+    spaceBetween: 40,
+    // autoplay: {
+    //     delay: 3000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+    },
+});
 });
